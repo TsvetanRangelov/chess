6f155bef264b8412a5a2b1904c800e498d37849e
TsvetanRangelov <c_rangelov@abv.bg>
2017-06-05 08:53:08 +0100

    Added full set of pieces

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -4,16 +4,49 @@ $(document).ready(function(){
   socket.on("username",function(username){
   });
   var field = $("#field");
+
+  //Set up White Pieces
   for(var i=0;i<16;++i){
     var d=$('<img>');
+    if(i>7) d.attr("piece", "Pawn");
+    if(i==0 || i==7) d.attr("piece", "Rook");
+    if(i==1 || i==6) d.attr("piece", "Knight");
+    if(i==2 || i==5) d.attr("piece", "Bishop");
+    if(i==3)d.attr("piece", "Queen");
+    if(i==4)d.attr("piece", "King");
+    d.attr("color", "White");
     d.addClass("ui-widget-content");
     field.append(d);
   }
 
-  $( "#field > .ui-widget-content" ).draggable();
+
+  //Set up Black Pieces
+  for(var i=0;i<16;++i){
+    var d=$('<img>');
+    if(i>7) d.attr("piece", "Pawn");
+    if(i==0 || i==7) d.attr("piece", "Rook");
+    if(i==1 || i==6) d.attr("piece", "Knight");
+    if(i==2 || i==5) d.attr("piece", "Bishop");
+    if(i==3)d.attr("piece", "Queen");
+    if(i==4)d.attr("piece", "King");
+    d.attr("color", "Black");
+    d.addClass("ui-widget-content");
+    field.append(d);
+  }
+
+  $( "#field > .ui-widget-content" ).draggable({
+    zIndex: 100
+  });
+
+  $("#field > .ui-widget-content ").on( "drag", function(event, ui) {
+    $(this).draggable("option", "zIndex", 200);
+  });
+
+
   $( "#field" ).droppable({
     drop: function( event, ui ) {
-      alert("Pawn to"+String.fromCharCode(parseInt(73-event.pageX/100, 10))+parseInt((event.pageY)/100+1,10));
+      let piece = ui.draggable;
+      alert(piece.attr("piece")+" to"+String.fromCharCode(parseInt(73-event.pageX/100, 10))+parseInt((event.pageY)/100+1,10));
     }
   });
 });
